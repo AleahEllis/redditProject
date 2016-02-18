91ae5519bcf536a28992de53f8bc2c82c836e999
Aleah Ellis <aleah.is@icloud.com>
2016-02-18 15:09:26 -0500

    created a loop for 1-10

--- reddit.js
+++ reddit.js
@@ -1,15 +1,50 @@
-$(function(){
-  
-  $.get('https://www.reddit.com/r/aww/.json', function(somethingElse){
-    var title = somethingElse.data.children[0].data.title;
+$(document).ready(function(){
+	
+
+
+
+ $.get('https://www.reddit.com/r/aww/.json', function(theInfo){
+ 	var children=theInfo.data.children
     
-    var dogStory = $("<h1></h1>").text(title);
+	$(children).each(function(i, post){
+
+		 var title = post.data.title;
+    
+    	var firstStory = $("<h1></h1>").text(title);
 
     
-    var url = somethingElse.data.children[0].data.thumbnail;
-    console.log(url);
-    var dogImage = $("<img />").attr("src", url);
-          $('body').append([dogStory, dogImage]);
-  });
+   			 var url = post.data.thumbnail;
+   // console.log(url);
+
+    var firstImage = $("<img />").attr("src", url);
+          $('#tiles').append([firstStory, firstImage]);
+ 	
+ 	
+		
+// console.log(post.data.title)
+
+
+	});
+
+
+
+	// 		for(var i=0; i<=10; i++){
+	// 			theInfo.push(i);
+	// 			$("#tiles").append(function(){
+	// 				return "<div class='bordbox'>" + i + "</div>";
+	// 			})
+	// 		}
+
+	// 	// $("div.available").click(function(){
+	// 	// $(this).toggleClass("reserved");
+
+	// });
+
+
+	});
+
+
+
+
   
 });
